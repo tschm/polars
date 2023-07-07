5e4a36672066087166d2a44c48f655b3619cf067
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-07-07 07:29:36 -0700

    install cmake

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-cargo --version
-rustc --version
+#cargo --version
+#rustc --version
 
-toolchain="nightly-2023-06-23"
+RUST_TOOLCHAIN := nightly-2023-06-23
+
+#RUST_TOOLCHAIN := $(RUST_TOOLCHAIN_VERSION)
+
+rustup toolchain install $(RUST_TOOLCHAIN) --component miri
+rustup default $(RUST_TOOLCHAIN)
+
+rustup component add rustfmt
+rustup component add clippy
+
 
 # follow https://github.com/pola-rs/polars/blob/main/CONTRIBUTING.md#setting-up-your-environment
-rustup toolchain install ${toolchain}-x86_64-unknown-linux-gnu --component miri
+#rustup toolchain install ${toolchain}-x86_64-unknown-linux-gnu --component miri
 
 # add https://github.com/rust-lang/rustfmt
-rustup component add rustfmt --toolchain ${toolchain}-x86_64-unknown-linux-gnu
-rustup component add clippy --toolchain ${toolchain}-x86_64-unknown-linux-gnu
+#rustup component add rustfmt --toolchain ${toolchain}-x86_64-unknown-linux-gnu
+#rustup component add clippy --toolchain ${toolchain}-x86_64-unknown-linux-gnu
 
 # Install dprint, see https://dprint.dev/install/
 # this will be slower since it builds from the source
 cargo install --locked dprint
-
-# install cmake
-sudo apt-get update && sudo apt-get install -y cmake
+cargo install cargo-license
